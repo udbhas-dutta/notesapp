3cb51cda4859f9fc284fe0c3aad91e168e4f19bc
Udbhas Dutta <udbhasdutta@gmail.com>
2023-10-27 23:57:24 +0530

    connected to mongodb and googleoauth20 using passport.js, added db.js and auth.js for handling the connections, added some more files to gitignore.

---
+++ server/routes/auth.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+
+//Google login route
+router.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/login-failure',
+        successRedirect: '/dashboard'
+    }),
+);
+
+//Route for unsuccessful login 
+router.get('/login-failure', async (req, res) => {
+    res.send("Something went wrong...")
+})
+
+module.exports = router;
