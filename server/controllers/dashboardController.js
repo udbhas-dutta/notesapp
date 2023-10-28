9c56d7dd867af008cd9171dcdc900a7362f12732
Udbhas Dutta <udbhasdutta@gmail.com>
2023-10-29 00:06:23 +0530

    added the layout for individual notes view, update and deletion

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -45,3 +45,26 @@ exports.dashboard = async (req, res) => {
     console.log(error);
   }
 };
+
+
+//GET /view specific note
+
+exports.dashboardViewNote = async(req,res) =>{
+  const note = await Note.findById({_id: req.params.id})
+  .where({user: req.user.id}).lean();
+
+  if(note) {
+    res.render('dashboard/view-note', {
+      noteID: req.params.id,
+      note, 
+      layout: '../views/layouts/dashboard'
+    });
+  } else {
+    res.send("Something went wrong.")
+  }
+}
+
+
+exports.dashboardUpdateNote = async(req,res) =>{
+  
+}
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -5,5 +5,7 @@ const { isLoggedIn } = require("../middleware/checkAuth");
 
 // dashboard routes
 router.get("/dashboard", isLoggedIn, dashboardController.dashboard);
+router.get("/dashboard/item/:id", isLoggedIn, dashboardController.dashboardViewNote);
+router.post("/dashboard/item/:id", isLoggedIn, dashboardController.dashboardUpdateNote);
 
 module.exports = router;
