66f8213fd5c0dca44586bf91626b2f3c7fbce962
Udbhas Dutta <udbhasdutta@gmail.com>
2023-10-28 16:32:48 +0530

    added the dashboard ui to show notes by hardcoding some samples and sending notes to the db-ejs file. Refactored db.ejs to show dashboard notes and added styling.

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,13 +1,26 @@
-//method : GET /dashboard
-exports.dashboard = async(req, res) => {
-    const locals = {
-        title: 'Dashboard',
-        description: 'Free Note Storage App'
-    }
+const Note = require("../models/Notes");
+const mongoose = require("mongoose");
 
-    
+//method : GET /dashboard
+exports.dashboard = async (req, res) => {
+  const locals = {
+    title: "Dashboard",
+    description: "Free Note Storage App",
+  };
+
+  try {
+    const notes = await Note.find({});
+
     res.render("dashboard/db-page", {
-        locals,
-        layout: '../views/layouts/dashboard'
-    })
-}
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: "../views/layouts/dashboard",
+    });
+    console.log(notes);
+  } catch (error) {
+    console.log(error);
+  }
+
+
+};
--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,7 @@ const UserSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now()
     },
 });
 
